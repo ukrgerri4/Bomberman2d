eddf1676706b5620d78ba42ada7a30f1142b5172
ukrgerri4 <ukrgerri4@gmail.com>
2025-03-04 01:55:48 +0200

    Save

--- Scenes/Bomb/Bomb.gd
+++ Scenes/Bomb/Bomb.gd
@@ -16,6 +16,7 @@ const EXPLOSIO_ANIMATION_NAME: String = "explosion"
 var _is_expoyded: bool = false
 var _explosion_timer: SceneTreeTimer = null
 var _explosion_delay: float = 3.0
+var _explosion_length: int = 2
 
 func _ready() -> void:
 	bomb_collision_shape.disabled = true
@@ -54,17 +55,71 @@ func _expoyded() -> void:
 	explosion_area.monitoring = true
 
 func _update_explosion_sprites() -> void:
-	pass
+	var start = ResourceManager.explosion_start.instantiate()
+	explosion_sprites.add_child(start)
+	start.global_position = global_position
+	
+	_update_explosion_beam(Vector2.UP)
+	_update_explosion_beam(Vector2.DOWN)
+	_update_explosion_beam(Vector2.LEFT)
+	_update_explosion_beam(Vector2.RIGHT)
+
+func _update_explosion_beam(direction: Vector2) -> void:
+	for i in range(_explosion_length):
+		var beam_number = i + 1
+		var beam_position = _get_beam_position(direction, beam_number)
+		var is_intersect = false # TODO: implement intersection check
+		if not is_intersect:
+			var top_beam: Sprite2D
+			if beam_number == _explosion_length:
+				top_beam = ResourceManager.explosion_end.instantiate()
+			else:
+				top_beam = ResourceManager.explosion_middle.instantiate()
+			explosion_sprites.add_child(top_beam)
+			top_beam.rotation_degrees = _get_beam_rotation(direction)
+			top_beam.global_position = beam_position
+
+func _get_beam_position(direction: Vector2, beam_number: int) -> Vector2:
+	match direction:
+		Vector2.UP:
+			return Vector2(global_position.x, global_position.y - Constants.BLOCK_SIZE * beam_number)
+		Vector2.DOWN:
+			return Vector2(global_position.x, global_position.y + Constants.BLOCK_SIZE * beam_number)
+		Vector2.LEFT:
+			return Vector2(global_position.x - Constants.BLOCK_SIZE * beam_number, global_position.y)
+		Vector2.RIGHT:
+			return Vector2(global_position.x + Constants.BLOCK_SIZE * beam_number, global_position.y)
+		_:
+			return Vector2.ZERO
+
+func _get_beam_rotation(direction: Vector2) -> int:
+	match direction:
+		Vector2.UP:
+			return -90
+		Vector2.DOWN:
+			return 90
+		Vector2.LEFT: 
+			return 180
+		Vector2.RIGHT:
+			return 0
+		_:
+			return 0
 
 func _update_explosion_animation() -> void:
-	var animation = animation_player.get_animation(EXPLOSIO_ANIMATION_NAME)
-	var parts = get_node("ExplosionSprites").get_children()
+	var animation_library = animation_player.get_animation_library("")
+	if animation_library.has_animation(EXPLOSIO_ANIMATION_NAME):
+		animation_library.remove_animation(EXPLOSIO_ANIMATION_NAME)
+	
+	var animation = Animation.new()
+	#var animation = animation_player.get_animation(EXPLOSIO_ANIMATION_NAME)
+	
+	var parts = explosion_sprites.get_children()
 	for part in parts:
 		var track_index = animation.add_track(Animation.TYPE_VALUE)
 		animation.track_set_path(track_index, str(part.get_path()) + ":frame")
 		for i in range(8):
 			animation.track_insert_key(track_index, 0.25 * i, i)
-	
+	animation_library.add_animation(EXPLOSIO_ANIMATION_NAME, animation)
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	#print_debug("Bomb animation ended: ", anim_name)
--- Services/ResourceManager.gd
+++ Services/ResourceManager.gd
@@ -3,6 +3,10 @@ extends Node
 const player_scene = preload("res://Scenes/Player/Player.tscn")
 const bomb_scene = preload("res://Scenes/Bomb/Bomb.tscn")
 
+const explosion_start = preload("res://Scenes/Bomb/Explosion/ExplosionStart.tscn")
+const explosion_middle = preload("res://Scenes/Bomb/Explosion/ExlosionMiddle.tscn")
+const explosion_end = preload("res://Scenes/Bomb/Explosion/ExplosionEnd.tscn")
+
 const player_textures = {
 	"white": preload("res://Assets/PlayerWhite.png"),
 	"black": preload("res://Assets/PlayerBlack.png"),
