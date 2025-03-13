a4d5d9e0dafbd85e8006d7d6cfbb030092428bf8
Igor Kryvenko <igor.kryvenko@materialise.kiev.ua>
2025-03-13 22:18:11 +0200

    Save

--- Scenes/MainScreen/MainScreen.gd
+++ Scenes/MainScreen/MainScreen.gd
@@ -1,2 +1,14 @@
 class_name MainScreen
 extends Control
+
+func _ready() -> void:
+	$PanelContainer/StartButton.pressed.connect(_on_start_button_pressed)
+	#$PanelContainer/SettingsButton.pressed.connect()
+	$PanelContainer/ExitButton.pressed.connect(_on_exit_button_pressed)
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_start_button_pressed() -> void:
+	self.visible = false
+	SceneManager.swap_scenes("res://Scenes/Game.tscn", get_node("/root/Main"), null, "no_transition")
